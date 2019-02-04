ee9559cb1de9e1c823dfa46605d5173670d876a8
Apurba <apurba10024@gmail.com>
2019-02-05 01:18:00 +0600

    1.2 Added Service And functionality Downolad Without Service

--- app/src/main/java/com/example/servicebrocastreceiving/MainActivity.java
+++ app/src/main/java/com/example/servicebrocastreceiving/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.servicebrocastreceiving;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "https://www.youtube.com/watch?v=-DYlmsTNjpw";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void downloadClicked(View view) {
+        CheckBox fake = findViewById(R.id.cb_fake);
+        EditText etdelay = findViewById(R.id.et_delay);
+        EditText etUrl = findViewById(R.id.et_url);
+
+        // without service
+        downloadWithoutService();
+    }
+
+    private void downloadWithoutService(){
+        Downloader.downloadFake(URL, 3000);
+        Toast.makeText(this, "Download Complete", Toast.LENGTH_SHORT).show();
     }
 }
