84c6bead4101e03fe8052a5e240d47d810bca1e7
Apurba <apurba10024@gmail.com>
2019-02-05 18:22:57 +0600

    1.6 Added Pop up Notification

--- app/src/main/java/com/example/servicebrocastreceiving/DownloaderIntentService.java
+++ app/src/main/java/com/example/servicebrocastreceiving/DownloaderIntentService.java
@@ -1,6 +1,9 @@
 package com.example.servicebrocastreceiving;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 
 import static com.example.servicebrocastreceiving.DownloaderService.DOWNLOAD_BROADCAST_ACTION;
@@ -22,6 +25,20 @@ public class DownloaderIntentService extends IntentService {
         done.setAction(DOWNLOAD_BROADCAST_ACTION);
         done.putExtra("url", url);
         sendBroadcast(done);
+
+        popUpNotification(url);
+    }
+
+    private void popUpNotification(String url){
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle("Download Complete")
+                .setContentText(url)
+                .setSmallIcon(R.drawable.ic_download);
+
+        Notification notification = builder.build();
+        NotificationManager manager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1234, notification);
     }
 
 }
